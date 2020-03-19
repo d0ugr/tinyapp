025ae67db7da20683fd5b6d7243b17d0a584b1de
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-19 15:23:19 -0400

    Added helper function comments

--- helpers.js
+++ helpers.js
@@ -3,6 +3,9 @@
 
 const ALPHANUMERIC = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
+// generateRandomString returns an ID string for use as a user ID or short URL key.
+//    TODO: Update to generate actual unique ID strings.
+
 const generateRandomString = function(length) {
 
   let result = "";
@@ -15,11 +18,17 @@ const generateRandomString = function(length) {
 
 };
 
+// getCurrentUser returns a user object for a given express HTTP request object containing a session.
+//    Returns undefined if no user is logged in.
+
 const getCurrentUser = function(users, req) {
 
   return users[req.session.userId];
 
 };
+
+// getUserByEmail returns a user object from the specified user database, given an email address ('username' given at login).
+//    Returns undefined if it is not found.
 
 const getUserByEmail = function(users, email) {
 
@@ -31,6 +40,10 @@ const getUserByEmail = function(users, email) {
 
 };
 
+// urlForUser returns a URL object for given user id and short URL key.
+//    The user and short URL are retrieved from a given Express HTTP request object.
+//    Returns the long URL, or undefined if no user is logged in, or the short URL key is invalid.
+
 const urlForUser = function(urls, req) {
 
   const url  = urls[req.params.shortURL];
@@ -41,6 +54,9 @@ const urlForUser = function(urls, req) {
   }
 
 };
+
+// urlsForUser returns an object containing a group of URL objects for the specified user.
+//    Returns an empty object if no matching URL objects were found.
 
 const urlsForUser = function(urls, userID) {
 
